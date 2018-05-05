5878ad66f024cbfe992e03c1749c6986df03c944
JordiZaplana <34708628+JordiZaplana@users.noreply.github.com>
2018-05-05 09:57:33 +0200

    version estable sensores falta testear millis --> 05/05/2018

--- Sensores_Shield/Sensores_Shield.ino
+++ Sensores_Shield/Sensores_Shield.ino
@@ -5,13 +5,17 @@
 #include <WiFi.h>
 #include <IOXhop_FirebaseESP32.h>
 
+#include <NTPClient.h>
+#include <WiFiUdp.h>/
+
+
 #define FIREBASE_HOST "https://tfg-app-v1.firebaseio.com"
 //#define FIREBASE_HOST "gs://tfg-app-v1.appspot.com"
 #define FIREBASE_AUTH "nf0th5wkgwCkUk5hfdJwVDrXKbjTsuUDm1qiy9wu"
-#define WIFI_SSID "MOVISTAR_2658"
-#define WIFI_PASSWORD "qu4yUXxq9tr44Ruwu4uu"
-//#define WIFI_SSID "AndroidAP"
-//#define WIFI_PASSWORD "11112222"
+//#define WIFI_SSID "MOVISTAR_2658"
+//#define WIFI_PASSWORD "qu4yUXxq9tr44Ruwu4uu"
+#define WIFI_SSID "AndroidAP"
+#define WIFI_PASSWORD "11112222"
 
 
 BME280 bme;
@@ -24,7 +28,7 @@ volatile unsigned long lastTick = 0;
 
 unsigned long tiempo1 = 0;
 unsigned long tiempo2 = 0;
-unsigned long tiempoApp = 0;
+int tiempoApp = 0;
 
 // Variables and constants used in tracking rainfall
 #define S_IN_DAY   86400
@@ -49,6 +53,10 @@ float windSpeed = 0.0;
 #define AIR_RST      4
 #define AIR_WAKE     15
 #define DONE_LED     5
+WiFiUDP ntpUDP;
+
+NTPClient timeClient(ntpUDP, "3.es.pool.ntp.org", 3600, 60000);
+
 
 /********************** setup ***********************/
 
@@ -78,6 +86,7 @@ void setup()
   bme.settings.humidOverSample = 1;
   bme.begin();
 
+
   // CCS811 sensor setup.
   pinMode(AIR_WAKE, OUTPUT);
   digitalWrite(AIR_WAKE, LOW);
@@ -102,7 +111,8 @@ void setup()
   Serial.println();
   Serial.print("connected: ");
   Serial.println(WiFi.localIP());
-
+  timeClient.begin();
+ 
   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
  
   //  Firebase.setInt("/placa_sensors/online", 1);
@@ -116,79 +126,39 @@ void setup()
 void loop()
 {
 
- 
-  static unsigned long outLoopTimer = 0;
-  static unsigned long wundergroundUpdateTimer = 0;
-  static unsigned long clockTimer = 0;
-  static unsigned long tempMSClock = 0;
-
-  // Create a seconds clock based on the millis() count. We use this
-  //  to track rainfall by the second. We've done this because the millis()
-  //  count overflows eventually, in a way that makes tracking time stamps
-  //  very difficult.
-  
-  
-  tiempoApp = Firebase.getString("/placa_sensors/millis").toInt();
-  tiempo2 = millis();
-//    Serial.print("Temperaturwedasdasfqae: " + Firebase.getString("/placa_sensors/millis"));
-    if(tiempo2 > (tiempo1 + tiempoApp)){  //Si ha pasado 1 segundo ejecuta el if
 
 
-  
-/*
 
 
-    // Update temperature. This also updates compensation values used to
-    //  calculate other parameters.
-    Serial.print("Temperature: ");
-    Serial.print(bme.readTempC(), 2);
-    Serial.println(" degrees ");
+String time1;
+int value_millis;
+timeClient.update();
+while(1){
 
-    // Display relative humidity.
-    Serial.print("% relative humidity: ");
-    Serial.print(bme.readFloatHumidity(), 2);
-    Serial.println(" %");
-
-    // Display pressure.
-    Serial.print("Pres: ");
-    Serial.print(bme.readFloatPressure() * 0.0002953);
-    Serial.println(" in");
-
-    // Trigger the CCS811's internal update procedure, then
-    //  dump the values to the serial port.
-    ccs.readAlgorithmResults();
-
-    Serial.print("CO2: ");
-    Serial.println(ccs.getCO2());
-
-    Serial.print("Luminous flux: ");
-    Serial.println(apds.readLuxLevel(), 6);
-  Firebase.setFloat("/placa_sensors/presion", bme.readFloatPressure() * 0.0002953);
-      Firebase.setFloat("/placa_sensors/co2", ccs.getCO2());
-      Firebase.setFloat("/placa_sensors/temperatura", bme.readTempC());
-      Firebase.setFloat("/placa_sensors/humidity", bme.readFloatHumidity());
-*/
+// millis control !!!!!!!!
   
 
-
-      Firebase.setFloat("/placa_sensors/lux", apds.readLuxLevel());
+    value_millis = Firebase.getInt("/placa_sensors/config/millis");
+          
+          //String time2 = String(timeClient.getDay()) + "-" + time1;          
       
-    tiempo1 = millis();
-    }
+      
+      Firebase.setFloat("/placa_sensors/lux"   ,apds.readLuxLevel());
+      Firebase.setFloat("/placa_sensors/presion" , bme.readFloatPressure() * 0.0002953);
 
-    /*
-      Firebase.pushInt("/placa_sensors/hist", windSpeed*1.492);
-      Firebase.pushInt("/placa_sensors/hist",apds.readLuxLevel());
-      Firebase.pushInt("/placa_sensors/hist", bme.readFloatPressure() * 0.0002953);
-      Firebase.pushInt("/placa_sensors/hist", ccs.getCO2());
-      Firebase.pushInt("/placa_sensors/hist", bme.readTempC());
-      Firebase.pushInt("/placa_sensors/hist", bme.readFloatHumidity());
-    */
+      Firebase.setFloat("/placa_sensors/temperatura" , bme.readTempC());
+      Firebase.setFloat("/placa_sensors/humidity" , bme.readFloatHumidity());
 
 
+      
+      Firebase.setInt("/placa_sensors/histSemanal/lux/" + time1  ,apds.readLuxLevel());
+       time1 = timeClient.getFormattedTime();
+    delay(value_millis);
+}
+           
 
 
-
+  
   }
 
 
